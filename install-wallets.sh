8dc5a33a544e3688f665850e29ca5a8ba400bd24
TJSull <terrancesullivanjr@gmail.com>
2016-02-15 20:04:00 +0000

    Update install-wallets.sh

--- install-wallets.sh
+++ install-wallets.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+echo "Installing Litecoin version 0.10.4.0 Wallet for Linux."
+echo "Running commands as root."
+
 #Required for Bitcoin and Litecoin Wallet GUIs
+echo "Installing libqtgui4"
 sudo apt-get install libqtgui4
 
 #Download Litecoin Wallet
+echo "Downloading the wallet..."
 wget https://download.litecoin.org/litecoin-0.10.4.0/linux/litecoin-0.10.4.0-linux64.tar.gz
 
 #Un-Tar the Wallet
 tar xzvf litecoin-0.10.4.0-linux64.tar.gz  
 
 #Change Directory
-cd litecoin-0.10.4.0-linux64/## 
+cd ./litecoin-0.10.4.0/bin/
 
 #Run the Wallet
+echo "Executing your newly installed wallet."
 ./litecoin-qt
